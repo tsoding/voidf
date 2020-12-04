cedff8ce2ad932f101f079ff853dfbba16e5f4c6
rexim <reximkut@gmail.com>
2020-12-04 19:50:36 +0700

    Implement SDL based counter UI

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
-CFLAGS=-Wall -Wextra -std=c11 -pedantic `pkg-config --cflags x11`
-LIBS=`pkg-config --libs x11`
+PKGS=x11 sdl2
+CFLAGS=-Wall -Wextra -std=c11 -pedantic `pkg-config --cflags $(PKGS)`
+LIBS=`pkg-config --libs $(PKGS)`
 
 voidf: main.c
 	$(CC) $(CFLAGS) -o voidf main.c $(LIBS)
--- main.c
+++ main.c
@@ -10,8 +10,84 @@
 #include <sys/ioctl.h>
 #include <linux/input.h>
 
+#include <SDL2/SDL.h>
+
 static const __u16 voidf[] = {KEY_V, KEY_O, KEY_I, KEY_D, KEY_F};
 static const size_t voidf_count = sizeof(voidf) / sizeof(voidf[0]);
+
+SDL_Texture *load_texture_from_bmp_file(SDL_Renderer *renderer,
+                                        const char *image_filepath,
+                                        SDL_Color color_key)
+{
+    SDL_Surface *image_surface = SDL_LoadBMP(image_filepath);
+
+    SDL_SetColorKey(
+        image_surface,
+        SDL_TRUE,
+        SDL_MapRGB(
+            image_surface->format,
+            color_key.r,
+            color_key.g,
+            color_key.b));
+
+    SDL_Texture *image_texture = SDL_CreateTextureFromSurface(renderer, image_surface);
+    SDL_FreeSurface(image_surface);
+    return image_texture;
+}
+
+#define BITMAP_FONT_ROW_SIZE    18
+#define BITMAP_FONT_CHAR_WIDTH  7
+#define BITMAP_FONT_CHAR_HEIGHT 9
+
+typedef struct {
+    SDL_Texture *bitmap;
+} Bitmap_Font;
+
+SDL_Rect bitmap_font_char_rect(Bitmap_Font *font, char x)
+{
+    if (32 <= x && x <= 126) {
+        const SDL_Rect rect = {
+            ((x - 32) % BITMAP_FONT_ROW_SIZE) * BITMAP_FONT_CHAR_WIDTH,
+            ((x - 32) / BITMAP_FONT_ROW_SIZE) * BITMAP_FONT_CHAR_HEIGHT,
+            BITMAP_FONT_CHAR_WIDTH,
+            BITMAP_FONT_CHAR_HEIGHT
+        };
+        return rect;
+    } else {
+        return bitmap_font_char_rect(font, '?');
+    }
+}
+
+Bitmap_Font bitmap_font_from_file(SDL_Renderer *renderer, const char *file_path)
+{
+    Bitmap_Font result = {0};
+    result.bitmap = load_texture_from_bmp_file(renderer, file_path, (SDL_Color) {0, 0, 0, 255});
+    return result;
+}
+
+void bitmap_font_render(Bitmap_Font *font,
+                        SDL_Renderer *renderer,
+                        int x, int y,
+                        int w, int h,
+                        SDL_Color color,
+                        const char *cstr)
+{
+    SDL_SetTextureColorMod(font->bitmap, color.r, color.g, color.b);
+    SDL_SetTextureAlphaMod(font->bitmap, color.a);
+
+    size_t n = strlen(cstr);
+
+    for (int col = 0; (size_t) col < n; ++col) {
+        const SDL_Rect src_rect = bitmap_font_char_rect(font, cstr[col]);
+        const SDL_Rect dest_rect = {
+            x + BITMAP_FONT_CHAR_WIDTH  * col * w,
+            y + BITMAP_FONT_CHAR_HEIGHT       * h,
+            src_rect.w * w,
+            src_rect.h * h
+        };
+        SDL_RenderCopy(renderer, font->bitmap, &src_rect, &dest_rect);
+    }
+}
 
 int is_voidf(size_t *cursor, __u16 x)
 {
@@ -65,6 +141,27 @@ void scan_devices(void)
 
 int main()
 {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "ERROR: Could not initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_Window * const window = SDL_CreateWindow(
+        "V̳͙̥̹̟͗̀̎̓͌͐́O̘̞͇̞̣͇͕͂͠I͙̋͐̍͂̀D̶͕̩̦̲͙F̟̖̮ͩ̏ͥ̂ͨ͠ ͍̰̫̯͙̯ͨ̉ͤ̈̿ͭI̤͍̲̯ͤ̎̀͝S̴̻͇̳̗̩ͧ̆ ̭̘̦ͭ͒Ĉ̸̰̼̤̖̲O̹̭̞̺̻͚̣̒M̪͓̗̤͋͢Ĩ͔̗̣̻̄̏̏̏̚N̳̦̂ͯ̅͂̓̈́G͈̣",
+        0, 0,
+        800, 600, SDL_WINDOW_RESIZABLE);
+    if (window == NULL) {
+        fprintf(stderr, "ERROR: Could not initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_Renderer *const renderer =
+        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        fprintf(stderr, "ERROR: Could not initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
     // TODO: filename is hardcoded
     const char *filename = "/dev/input/event14";
 
@@ -74,23 +171,63 @@ int main()
         exit(1);
     }
 
+    {
+        int flags = fcntl(fd, F_GETFL, 0);
+        fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+    }
+
+    // TODO: charmap-oldschool.bmp should be baked into the executable
+    Bitmap_Font font = bitmap_font_from_file(renderer, "./charmap-oldschool.bmp");
+
     int quit = 0;
     struct input_event ev[64];
     size_t cursor = 0;
+    int voidf_count = 0;
+    char voidf_buffer[1024];
     while (!quit) {
-        int rd = read(fd, ev, sizeof(ev));
-        size_t n = rd / sizeof(struct input_event);
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT: {
+                quit = 1;
+            } break;
+            }
+        }
 
-        for (size_t i = 0; i < n; ++i) {
-            if (ev[i].type == EV_KEY && ev[i].value == 0) {
-                if(is_voidf(&cursor, ev[i].code)) {
-                    printf("VOIDF IS COMING\n");
+        int rd = read(fd, ev, sizeof(ev));
+
+        if (rd > 0) {
+            size_t n = rd / sizeof(struct input_event);
+
+            for (size_t i = 0; i < n; ++i) {
+                if (ev[i].type == EV_KEY && ev[i].value == 0) {
+                    if(is_voidf(&cursor, ev[i].code)) {
+                        printf("VOIDF IS COMING\n");
+                        voidf_count += 1;
+                    }
                 }
             }
         }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        snprintf(voidf_buffer, sizeof(voidf_buffer), "%d", voidf_count);
+
+        // TODO: voidf counter is ugly
+        bitmap_font_render(&font,
+                           renderer,
+                           0, 0,
+                           10, 10,
+                           (SDL_Color) {255, 255, 255, 255},
+                           voidf_buffer);
+
+        SDL_Delay(10);
+        SDL_RenderPresent(renderer);
     }
 
     close(fd);
+    SDL_Quit();
 
     return 0;
 }
